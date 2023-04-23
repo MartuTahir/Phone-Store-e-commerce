2100b2bcc1f583d5c7a94ef44a62a84bcf0419d2
Martina Tahir <martinanicoleth@gmail.com>
2023-04-22 21:27:41 -0300

    carrito con mejoras

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ let btn = document.querySelector("#btn-carro")
 let cantidadCarrito
 const compras = document.querySelector('.modal-body')
 const precioCarrito = document.querySelector(`.precio`)
+let eliminar = document.querySelector('.btn-eliminar')
+let vaciar = document.querySelector('.vaciar')
 //creacion de cards de productos 
 const gridCards = document.querySelector(`.grid-card`);
 
@@ -25,6 +27,13 @@ productos.forEach((productos) => {
     `
     boton.addEventListener('click', () => {
         agregarProducto(id)
+        swal.fire({
+            icon: 'success',
+            title: "¡Tu producto ya está en el carrito!",
+            showConfirmButton: false,
+            toast: true,
+            timer: 1500
+        })
     })
     div.append(boton)
     gridCards.appendChild(div)
@@ -46,15 +55,58 @@ function agregarProducto(id) {
 const actualizarCarrito = () => {
     compras.innerHTML = ''
     carrito.forEach((prod) => {
+        const {nombre, id, color, precio, img} = prod
         const div = document.createElement("div")
+        div.className = 'contenedor-prod-carrito'
         div.innerHTML = `
-            <img src="${prod.img}" class="img-prodCa" ></img>
-            <p>${prod.nombre}</p>
-            <p>Precio: $${prod.precio}</p>
-            <p> <span id="cantidad"> Cantidad: </span></p>
-            <button  id="btnElim" class="btn-eliminar">Borrar</button>
+            <img src="${img}" class="img-prodCa" ></img>
+            <p class="text-prod" >${nombre}</p>
+            <p class="text-prod">Precio: $${precio}</p>
         `
+        const botonBorrar = document.createElement('button')
+        botonBorrar.className = "btn-eliminar"
+        botonBorrar.innerHTML = `<img class="trash" src="./images/trash-svgrepo-com.svg">`
+        botonBorrar.addEventListener('click', () => {
+            borrarProd(id)
+            Swal.fire({
+                iconHtml: '<img class="trash2" src="./images/trash2-svgrepo-com.svg">',
+                title: 'Producto borrado',
+                toast: true,
+                showConfirmButton: false,
+                timer: 1500
+              })
+        })
+        div.append(botonBorrar)
         compras.append(div)
+    })
+    vaciar = document.querySelector('.vaciar')
+    vaciar.addEventListener("click", () => {
+        Swal.fire({
+            title: '¿Estas seguro de vaciar el carrito?',
+            toast: true,
+            customClass: {
+                title: 'title-alert'
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Si',
+            confirmButtonColor: '#19695f',
+            cancelButtonText: `Cancelar`,
+            cancelButtonColor: 'rgb(155, 18, 18)',
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                icon: 'success',
+                title: 'Se vació tu carrito',
+                toast: true,
+                showConfirmButton: false,
+                timer: 1500
+              })
+              carrito = []
+              compras.innerHTML = 'Tu carrito esta vacio'
+              totalCarrito()
+              guardarStorage()
+            }
+          })
     })
     totalCarrito()
     guardarStorage()
@@ -64,6 +116,21 @@ const actualizarCarrito = () => {
 const totalCarrito = () => {
     precioCarrito.innerText = 'Total: $' + carrito.reduce((acc, producto) => acc += producto.precio, 0)
 }
+
+//elimina productos del carrito
+
+function borrarProd(id) {
+    console.log(id);
+    const prodEliminado = carrito.find((prod) => prod.id === id);
+    console.log(prodEliminado)
+    console.log(carrito);
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== prodEliminado
+    })
+    console.log(carrito);
+    actualizarCarrito()
+}  
+
 
 //guarda productos y cantidad de productos en el local storage
 
